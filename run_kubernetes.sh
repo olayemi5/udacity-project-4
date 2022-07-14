19bbe2035f68ee738b927ae2442a475744d83471
Stephen Olayemi <stephen.olayemi@innotelligent.com>
2022-07-14 12:41:11 +0100

    Work in progress

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath='solar007/udaictiy-project-4'
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udaictiy-project-4 --image=$dockerpath --port=8080
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udaictiy-project-4 8080:80
 
